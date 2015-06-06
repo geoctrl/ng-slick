6e3ded5b34003fe57cb012f9178a2ee0657791cc
Tony <tonylefler@gmail.com>
2015-06-06 01:49:56 -0600

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt){
+
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            livereload: {
+                options: {livereload: true},
+                files: [
+                    'components/**/*'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        ''
+                    ]
+                }
+            }
+        }
+
+    });
+
+    grunt.registerTask('serve', function (target) {
+
+        grunt.task.run([
+            'connect:livereload',
+            'watch'
+        ]);
+    });
+
+
+    grunt.registerTask('default', []);
+};
